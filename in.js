3d2e1e0aadcad8cb020514b57971c03d5c8cc4fd
GuoKai <badkaikai@gmail.com>
2011-07-13 15:17:56 +0800

    added In.ready() method to loading queue after contentLoaded...

--- in.js
+++ in.js
@@ -135,7 +135,7 @@
 	}
 	
 	//in - public
-	__in=function() {
+	var __in=function() {
 		//empty
 		var args=[].slice.call(arguments);
 		
@@ -153,6 +153,46 @@
 		var stack=new stackline(blahlist);
 		stack.start();
 	};
+	
+	//contentLoaded
+	function contentLoaded(win,fn) {
+		var done=false,top=true,
+		doc=win.document,root=doc.documentElement,
+		add=doc.addEventListener ? 'addEventListener':'attachEvent',
+		rem=doc.addEventListener ? 'removeEventListener':'detachEvent',
+		pre=doc.addEventListener ? '':'on',
+		
+		init=function(e) {
+			if(e.type=='readystatechange' && doc.readyState!='complete') return;
+			(e.type=='load' ? win:doc)[rem](pre+e.type,init,false);
+			if(!done && (done=true)) fn.call(win,e.type || e);
+		},
+		
+		poll=function() {
+			try {root.doScroll('left');} catch(e) {setTimeout(poll,50);return;}
+			init('poll');
+		};
+		
+		if (doc.readyState=='complete') {
+			fn.call(win,'lazy');
+		} else {
+			if (doc.createEventObject && root.doScroll) {
+				try {top=!win.frameElement;} catch(e) {}
+				if(top) poll();
+			}
+			doc[add](pre+'DOMContentLoaded',init,false);
+			doc[add](pre+'readystatechange',init,false);
+			win[add](pre+'load',init,false);
+		}
+	}
+	
+	//in - ready
+	var __ready=function() {
+		var args=arguments;
+		contentLoaded(window,function() {
+			__in.apply(this,args);
+		});
+	}
 	
 	//in - initialize
 	~function() {
@@ -175,5 +215,6 @@
 	__in.exe=__in;
 	__in.load=__load;
 	__in.add=__add;
+	__in.ready=__ready;
 	this.In=__in;
 }();
